a744514a9f3ae30d3161f550818069845ddf1c20
Brian Fuentes <Brian.Fuentes04@gmail.com>
2023-07-27 15:37:31 -0700

    working signup

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const todos = require('./routes/todoRoute')
+const userRoute = require('./routes/userRoute')
 
 // express app
 const app = express()
@@ -19,6 +20,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/todo', todos)
+app.use('/api/user', userRoute)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
